ff6f15d1b8e98ca82440afe6225e2979f0e67d7d
tdkt21 <kotadesu2124@gmail.com>
2024-06-25 16:00:39 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+for (let num = 1; num <= 50; num++) {
+
+if (num % 3 === 0) {
+    console.log('3の倍数です');
+}
+if (num % 5 === 0) {
+    console.log('5の倍数です');
+}
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log('3と5の倍数です');
+}
+else {
+    console.log(num);
+}
+
+}
